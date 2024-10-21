3cd80f57fb4960f1e0606cb573f24f8c68a7a85a
QuocChung <tuquocchung@gmail.com>
2024-10-22 03:19:26 +0700

    Lần 1

---
+++ Controllers/HomeAPIController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MuoiHai_Limupa.Models;
+
+namespace MuoiHai_Limupa.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeAPIController : ControllerBase
+    {
+        public QlthuVienContext db =new QlthuVienContext();
+
+        [HttpGet("{maNXB}")]
+        public IEnumerable<TSach> LaySachTheoMa(string maNXB)
+        {
+            var list_sach = db.TSaches.Where(x => x.MaNxb ==maNXB).ToList();
+            return list_sach;
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MuoiHai_Limupa.Models;
+using System.Diagnostics;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace MuoiHai_Limupa.Controllers
+{
+    public class HomeController : Controller
+    {
+        public QlthuVienContext db = new QlthuVienContext();
+
+        public IActionResult Index()
+        {
+            ViewBag.TenNhaXuatBan = db.TNhaXbs.ToList();
+            var list_docGia = db.TDocGia.ToList();
+            return View(list_docGia);
+        }
+
+        [Route("ThemSachMoi")]
+        [HttpGet]
+
+        public IActionResult ThemSachMoi()
+        {
+            ViewBag.MaLoai = new SelectList(db.TLoaiSaches.ToList(), "MaLoai", "TenLoai");
+            ViewBag.MaNgonNgu = new SelectList(db.TNgonNgus.ToList(), "MaNgonNgu", "TenNgonNgu");
+            ViewBag.MaNxb = new SelectList(db.TNhaXbs.ToList(), "MaNxb", "TenNxb");
+            ViewBag.TenNhaXuatBan = db.TNhaXbs.ToList();
+            return View();
+        }
+
+        [Route("ThemSachMoi")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult ThemSachMoi(TSach Sach)
+        {
+            /*- == true thì chứng tỏ dứ liệu truyền từ form vào hợp lệ -*/
+            if (ModelState.IsValid)
+            {
+                db.Add(Sach);
+                db.SaveChanges();
+                return RedirectToAction("Index"); // Chuyển hướng về trang danh sách
+            }
+            ViewBag.TenNhaXuatBan = db.TNhaXbs.ToList();
+            return View();
+        }
+
+
+    }
+}
+
+
